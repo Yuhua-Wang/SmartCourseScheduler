2ecfab2df975e31c307ca57ef6d089adec08db2b
YijiaZhanghh <tyouiiika@gmail.com>
2019-06-20 18:04:45 -0700

    rate my prof

---
+++ src/rmp/rmpScrapper.java
@@ -0,0 +1,31 @@
+package rmp;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class rmpScrapper {
+
+    //Get the Name, University, and Faculty of the professor
+    public String getProfInfo() throws IOException {
+        Document dc = Jsoup.connect("https://www.ratemyprofessors.com/search.jsp?query="+"Gateman+Robert").get();
+        Elements body = dc.getElementsByClass("listing PROFESSOR");
+        Elements link = body.select("a[href]"); //Link of the professor
+        System.out.println(link.first().text()); //Print the information of the first professor (the most relevant)
+        return link.attr("href");
+    }
+
+    //Get the overall quality and level of difficulty of the professor
+    public void getProfScore() throws IOException {
+        Document dc = Jsoup.connect("https://www.ratemyprofessors.com"+ getProfInfo()).get();
+        Elements overallQuality = dc.getElementsByClass("breakdown-container quality");
+        Elements levelDifficulty = dc.getElementsByClass("breakdown-section difficulty");
+        System.out.println(overallQuality.text());
+        System.out.println(levelDifficulty.text());
+    }
+
+
+}
