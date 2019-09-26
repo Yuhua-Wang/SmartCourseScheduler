4404695c08dde1589da7ac1d462ddbb4a7c07d29
Dawn Rising <yuhuawang0601@outlook.com>
2019-09-25 22:03:45 -0700

    fixed a bug in UI where Components does not show up if frame size is not changed

---
+++ src/UI/TimeTableWindow.java
@@ -0,0 +1,26 @@
+package UI;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class TimeTableWindow extends UI {
+
+
+    public TimeTableWindow() throws IOException {
+        super();
+    }
+
+    private void initializeTable(){
+
+    }
+    @Override
+    protected void initializeButton() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
--- src/UI/UI.java
+++ src/UI/UI.java
@@ -27,6 +27,7 @@ public abstract class UI implements ActionListener {
         initializeDialog();
     }
 
+
     protected  void initializeFrame() {
         frame = new JFrame("Simple Todo List");
         frame.setSize(frameWidth,frameHeight);
@@ -76,7 +77,7 @@ public abstract class UI implements ActionListener {
         button.setBackground(Color.WHITE);
         button.setOpaque(true);
         frame.add(button);
-        determineDimension(frame, button, x_pos, y_pos, width, height);
+        setLocation(frame, button, x_pos, y_pos, width, height);
         //allowResize(frame, button, x_pos, y_pos, width, height);
 
         return button;
@@ -87,27 +88,35 @@ public abstract class UI implements ActionListener {
     // x_pos, y_pos are NOT coordinates. They mean which part on the frame should the center of the component be at.
     // width and height are NOT the component's actual width and height. They mean what portion of the frame's x y should the component occupy
     // e.g. x_pos = 0.5, y_pos = 0.5 means the button should be at the center of the screen
-    private void determineDimension(JFrame frame, JComponent component, Double x_pos, Double y_pos, Double width, Double height){
+    private void setLocation(JFrame frame, JComponent component, Double x_pos, Double y_pos, Double width, Double height){
+        int[] dimension = determineDimention(frame, component, x_pos, y_pos, width, height);
+        component.setBounds(dimension[0], dimension[1], dimension[2], dimension[3]);
         frame.addComponentListener( new ComponentAdapter() {
             @Override
             public void componentResized( ComponentEvent e ) {
-                int[] dimension = new int[4];
-                int frameWidth = (int) Math.round(frame.getBounds().getWidth());
-                int frameHeight = (int) Math.round(frame.getBounds().getHeight());
-                dimension[2] = (int) Math.round(frameWidth * width);
-                dimension[3] = (int) Math.round(frameHeight * height);
-                dimension[0] = (int) Math.round(frameWidth*x_pos) - dimension[2]/2;
-                dimension[1] = (int) Math.round(frameHeight*y_pos) - dimension[3]/2;
+                int[] dimension = determineDimention(frame, component, x_pos, y_pos, width, height);
                 component.setBounds(dimension[0], dimension[1], dimension[2], dimension[3]);
             }
         } );
+    }
+
+    // dimension[0] = x coordinate, dimension[1] = y coordinate, [2] =width, [3] = height
+    protected int[] determineDimention(JFrame frame, JComponent component, Double x_pos, Double y_pos, Double width, Double height) {
+     int[] dimension = new int[4];
+     int frameWidth = (int) Math.round(frame.getBounds().getWidth());
+     int frameHeight = (int) Math.round(frame.getBounds().getHeight());
+     dimension[2] = (int) Math.round(frameWidth * width);
+     dimension[3] = (int) Math.round(frameHeight * height);
+     dimension[0] = (int) Math.round(frameWidth*x_pos) - dimension[2]/2;
+     dimension[1] = (int) Math.round(frameHeight*y_pos) - dimension[3]/2;
+     return dimension;
     }
 
     protected JLabel createLabel(String label, Double x, Double y,Double width, Double height, int size){
         JLabel jLabel = new JLabel(label);
         jLabel.setFont(new Font("Serif",Font.PLAIN,size));
         frame.add(jLabel);
-        determineDimension(frame, jLabel, x, y,width,height);
+        setLocation(frame, jLabel, x, y,width,height);
 
         return jLabel;
     }
@@ -116,7 +125,7 @@ public abstract class UI implements ActionListener {
         JTextField textField = new JTextField(text);
         textField.setFont(new Font("Serif",Font.PLAIN,size));
         frame.add(textField);
-        determineDimension(frame, textField, x, y,width,height);
+        setLocation(frame, textField, x, y,width,height);
 
         return textField;
     }
